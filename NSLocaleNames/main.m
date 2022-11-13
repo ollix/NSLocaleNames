d46738805a81171d81de92ce5a43516034a021fb
Olli Wang <olliwang@ollix.com>
2022-11-13 12:04:51 +0800

    Initial Commit

---
+++ NSLocaleNames/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSLocaleNames
+//
+//  Created by Olli Wang on 2022/11/13.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
